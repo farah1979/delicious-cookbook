44cf3473bd1b0f23270f9ab909736a6473cd8937
farah1979 <farahhisham1979@gmail.com>
2021-07-09 17:11:16 +0000

    Add register functionality and login functionality

--- app.py
+++ app.py
@@ -31,7 +31,54 @@ def recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        exist_user = mongo.db.users.find_one(
+                              {"first_name": request.form.get("first_name").lower(),
+                               "last_name": request.form.get("last_name").lower()})
+
+        if exist_user:
+            flash("The User Is Already Exists")
+            return redirect(url_for("register"))
+
+        register_user = {
+            "first_name": request.form.get("first_name").lower(),
+            "last_name": request.form.get("last_name").lower(),
+            "e_mail": request.form.get("e_mail"),
+            "password": generate_password_hash(request.form.get("password")) 
+        } 
+        mongo.db.users.insert_one(register_user)
+
+        session["user"] = (request.form.get("first_name").lower(),
+                           request.form.get("last_name").lower())
+
+        flash("You have been successfully registered")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        exist_user = mongo.db.users.find_one(
+                              {"e_mail": request.form.get("e_mail").lower()})
+
+        if exist_user:
+            #Check Password Matches
+            if check_password_hash(
+                    exist_user["password"], request.form.get("password")):
+                    session["user"] = (request.form.get("e_mail").lower())
+                    flash("Welcome, {}".format(request.form.get("e_mail")))
+
+            else:
+                #if the password not matches
+                flash("email id and/or password is incorrect")
+                return redirect(url_for("login"))
+
+        else:
+            #The user's email can not found
+            flash("email id and/or password is incorrect")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
